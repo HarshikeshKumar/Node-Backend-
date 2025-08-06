e6ec6ab3156610fe741dd76c785e00eba75bfb1f
HarshikeshKumar <harshikeshkoili214@gmail.com>
2025-08-07 01:13:13 +0530

    Added some routing in server

--- 1_filesystem/server.js
+++ 1_filesystem/server.js
@@ -1,7 +1,17 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello World");
+  // console.log(req.url);
+
+  if (req.url === "/home") {
+    res.end("This is home page");
+  } else if (req.url === "/about") {
+    res.end("This is About Page");
+  } else if (req.url === "/name") {
+    res.end("My Name is Harshikesh Kumar");
+  } else {
+    res.end("Hello World");
+  }
 });
 
 server.listen(3000, () => {

4184522780c21f734515259af0b64ba6634595b4
HarshikeshKumar <harshikeshkoili214@gmail.com>
2025-08-06 23:50:36 +0530

    HandlebarsTempletedHTML

--- 1_filesystem/readFile.mjs
+++ 1_filesystem/readFile.mjs
@@ -1,10 +1,20 @@
-import { readFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
 import { resolve } from "path";
 
 async function readingHTML() {
   const path = resolve("./index.html");
-  const file = await readFile(path);
-  console.log(file.toString());
+  let file = await readFile(path);
+
+  const data = {
+    name: "Harshikesh Kumar",
+    course: "MERN Stack Development",
+    institute: "Physics Wallah",
+  };
+
+  for (const [key, value] of Object.entries(data)) {
+    file = file.toString().replace(`{{${key}}}`, value);
+  }
+  await writeFile(resolve("./output.html"), file);
 }
 
 readingHTML();
